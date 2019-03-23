e8f105328f8808f041d184c135e41e2a2cee3eee
sigvesn <sigve@samfundet.no>
2019-03-23 10:45:13 +0100

    pimp my stats

--- plenty_runs/generate_best_user.sh
+++ plenty_runs/generate_best_user.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-find . -printf '%p\n' -name "*.txt" -exec sed -n 212,1p {} \; > best_user_stats.txt
+find . -name "stats*.txt" -printf '%p\n' -exec sed -n 212,1p {} \; | sed 's/\.\/stats_\|user\|\.txt//g' | awk '{ if(length($1) > 5) printf "%12s -> ", $1; else print $1 }' > best_user_stats.txt
+sort -n -k 3 -o best_user_stats.txt best_user_stats.txt
