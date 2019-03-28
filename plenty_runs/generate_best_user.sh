ddc2958a1107dd012cae56c4fe1c7c0ac676d715
sigvesn <sigve@samfundet.no>
2019-03-28 14:25:13 +0100

    update script

--- plenty_runs/generate_best_user.sh
+++ plenty_runs/generate_best_user.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-find . -name "stats*.txt" -printf '%p\n' -exec sed -n 212,1p {} \; | sed 's/\.\/stats_\|user\|\.txt//g' | awk '{ if(length($1) > 5) printf "%12s -> ", $1; else print $1 }' > best_user_stats.txt
-sort -n -k 3 -o best_user_stats.txt best_user_stats.txt
+for i in $(seq 3 15); do
+    for j in $(seq 2 4); do
+        let ts=$i*20
+        let dw=$j*5
+        echo $ts $dw
+
+        find . -name "stats_${ts}_*_*_${dw}*.txt" -printf '%p\n' -exec sed -n 212,1p {} \; | sed 's/\.\/stats_\|user\|\.txt//g' | awk '{ if(length($1) > 5) printf "%s ", $1; else print $1 }' | sed 's/_/ /g' > best_user_stats${ts}_${dw}.txt
+
+        sort -n -k 3 -o best_user_stats${ts}_${dw}.txt best_user_stats${ts}_${dw}.txt
+    done
+done
